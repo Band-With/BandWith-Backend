7f562c4ebdbd663c426ddf22011b591d2cd61578
kyeongmin97 <kio1015@naver.com>
2021-04-27 02:08:52 +0900

    Add a function downloading the S3 file

--- src/main/java/com/bandwith/service/S3Service.java
+++ src/main/java/com/bandwith/service/S3Service.java
@@ -1,9 +1,13 @@
 package com.bandwith.service;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface S3Service {
-    String[] fileUpload(String path, MultipartFile file);
+    String[] fileUpload(String path, MultipartFile file) throws IOException;
+    Resource fileDownload(String key) throws IOException;
     void fileDelete(String path);
     String getFileURL(String path);
     void createFolder(String folderName);
