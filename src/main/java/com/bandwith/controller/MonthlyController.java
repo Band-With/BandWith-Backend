bbbcf66702f3084e55cf08f4882cb6a9161d563b
eastcolumn <kimdj1026@naver.com>
2021-06-05 20:23:34 +0900

    cron setting

--- src/main/java/com/bandwith/controller/MonthlyController.java
+++ src/main/java/com/bandwith/controller/MonthlyController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "*", allowCredentials = "true")
 @RestController
 public class MonthlyController {
     private ScheduledService scheduledService;
